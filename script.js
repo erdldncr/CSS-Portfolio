27ae6c834add3fc8f9a8c5f88890940395f75c81
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-13 01:43:19 +0000

    project has been completed

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active')
     mobile_menu.classList.toggle('active')
 })
+document.addEventListener('scroll',()=>{
+let scrollPosition=window.scrollY
+if(scrollPosition>250){
+    header.style.backgroundColor='#29323c'
+}else{
+    header.style.backgroundColor='transparent'
+}
+
+})
